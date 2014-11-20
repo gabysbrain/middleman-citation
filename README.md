443cce42014da6b5eb17e491c1e1e1ad82f819a6
J.J. Green <j.j.green@gmx.co.uk>
2014-11-20 00:01:36 +0000

    initial changes & documentation, not yet tested

--- README.md
+++ README.md
@@ -21,31 +21,56 @@ Or install it yourself as:
 
     $ gem install middleman-citation
 
-
 ## Configuration
 
 In your `config.rb` file for your site add:
 
     require 'middleman-citation'
 
-    activate :citation do |options|
-      options.bibtex = "/Users/tom/Dropbox/Research/all.bib" # full path to a bibtex file
-      options.style = "ieee" # or whatever style you want from citeproc-styles
-    end
+    activate :citation do |opts|
+      opts.bibtex = '/path/to/your.bib' # path to a bibtex file
+      opts.style  = 'ieee'              # style from citeproc-styles
+	  opts.format = 'html'              # output format
+	end
 
 ## Usage
 
 This adds the following helper methods you can use in your Middleman
 templates.
 
+* `citations_search(search_term, author)`: Search the BibTeX file for all
+	citations matching a search term (such as `'@article'`) and by the given
+	author.  The `author` argument can be ommitted to match all authors and
+	a `search_term` of `nil` will match all items in the bibliography.
 
+* `citation(key)`: Given a BibTeX citation key as returned from
+  `citations_search`, return a formatted string the citation according to
+  how the `style` and `format` options were set.
 
-* `cite_full(key)`: given the BibTeX citation key return a formatted string
-                  for the citation according to how the `style` option is set.
-* `find_pubs(search_key, author)`: Search the BibTeX file for all citations 
-                                 of a certain type for a # certain author.  
-                                 Returns a list of citation keys you can send 
-                                 to `cite_full`.
+For extra control on the output, one can use:
+
+* `citation_entry(key)`: Return the unformatted entry (a hash)
+  corresponding to the BibTeX citation key. 
+
+* `citation_formatted(entry)`: Format an unformatted entry.
+
+In fact the `citation` method is implemented using these:
+
+	def citation(key)
+	  entry = citation_entry(key)
+	  citation_formatted(entry)
+    end
+
+The point is that one can interrogate the unformatted entry to
+add extra formatting: The following code adds a DOI link if the
+entry matching the `key` has a `URL` field containing the DOI.
+
+	entry      = citation_entry(key)
+	entry_html = citation_formatted(entry)
+	if doi_url = entry.fetch('URL', false) then
+      doi_link = "(%s)" % link_to('doi', doi_url)
+    end
+	[entry_html, doi_link].compact.join(' ')
 
 ## Contributing
 
--- lib/middleman-citation.rb
+++ lib/middleman-citation.rb
@@ -1,48 +1,60 @@
-require "middleman-citation/version"
-require "middleman-core"
-require "bibtex"
-require "citeproc"
+require 'middleman-citation/version'
+require 'middleman-core'
+require 'bibtex'
+require 'citeproc'
 
 module Middleman
+
   class CitationExtension < Middleman::Extension
-    option :bibtex, "", "The path to the bibtex file to use"
-    option :style, "chicago-author-date", "The citation style to use"
-  
+
+    option :bibtex, nil,                   'Path to the bibtex file'
+    option :style,  'chicago-author-date', 'Citation style'
+    option :format, 'html',                'Citation format'
+
     def initialize(app, options_hash={}, &block)
       super
-  
-      @@bibtex = BibTeX.open(options.bibtex)
-      # for some reason the options function is the one from 
-      # middleman-deploy in the helper methods below...
-      @@cite_style = options.style
+      app.set(:bibtex,      BibTeX.open(options.bibtex))
+      app.set(:cite_style,  options.style)
+      app.set(:cite_format, options.format)
     end
   
     helpers do
-      # Given a BibTeX citation return a formatted string according to
-      # how the setting of the style option
-      # Params:
-      # +key+:: bibtex key located in the BibTeX file defined in the config
-      def cite_full(key)
-        # need to convert latex special characters, like {\"o} to unicode
-        @@bibtex[key].convert_latex
-        CiteProc.process(@@bibtex[key].to_citeproc, :style => @@cite_style)
+
+      def search_citations(search_key, author = nil)
+        entries_matching_key = bibtex.query(search_key)
+        entries = 
+          if author then
+            search_by_author(entries_matching_key, author)
+          else
+            entries_matching_key
+          end
+        entries.sort { |x, y| y.year.to_i <=> x.year.to_i }.map(&:key)
       end
-  
-      # Search the BibTeX file for all citations of a certain type for a 
-      # certain author.  Returns a list of citation keys you can send to
-      # +cite_full+.
-      # Params:
-      # +search_key+:: filter keys for the BibTeX file, e.g. @book, @article,
-      #                etc.
-      # +author+:: The full author name to search for
-      def find_pubs(search_key, author)
-        bib_name = BibTeX::Name.parse(author)
-        results = @@bibtex.query(search_key) do |e|
-          e.respond_to?(:author) and e.author and e.author.include? bib_name
+
+      def citation_entry(key)
+        bibtex[key].convert_latex.to_citeproc
+      end
+
+      def citation_formatted(entry)
+        CiteProc.process(entry, :style => cite_style, :format => cite_format)
+      end
+
+      def citation(key)
+        entry = citation_entry(key)
+        citation_formatted(entry)
+      end
+
+      private
+
+      def search_by_author(entries, author)
+        bib_author = BibTeX::Name.parse(author)
+        entries.select do |e|
+          e.respond_to?(:author) && 
+            e.author && 
+            e.author.include?(bib_author)
         end
-        results.sort! {|x,y| y.year.to_i-x.year.to_i}
-        results.map {|x| x.key}
       end
+
     end
   end
 end
