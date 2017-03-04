73945e3696a746850f536ff8ee6c9b5bcf4592f9
Hermann Detz <hermann.detz@tuwien.ac.at>
2017-03-04 16:19:37 +0100

    New functions: citations_load_bibtex_file and citations_list_years. New parameter for cite_full function, which adds a link to the citation.

--- lib/middleman-citation.rb
+++ lib/middleman-citation.rb
@@ -13,7 +13,9 @@ module Middleman
 
     def initialize(app, options_hash={}, &block)
       super
-      app.config[:bibtex] = BibTeX.open(options.bibtex, :filter => 'latex')
+      app.config[:bibtex] = BibTeX.open(options.bibtex)
+      app.config[:bibtex].replace
+      app.config[:bibtex].convert(:latex)
       app.config[:cite_style] = options.style
       app.config[:cite_format] = options.format
     end
@@ -23,8 +25,8 @@ module Middleman
       # how the setting of the style option
       # Params:
       # +key+:: bibtex key located in the BibTeX file defined in the config
-      def cite_full(key)
-        Citations::cite_full(key, config.bibtex, config.cite_style, config.cite_format)
+      def cite_full(key,link=false)
+        Citations::cite_full(key, config.bibtex, config.cite_style, config.cite_format,link)
       end
 
       # Given a BibTeX citation return a block containing the citation
@@ -49,6 +51,30 @@ module Middleman
         end
 
         entries.sort { |x, y| y.year.to_i <=> x.year.to_i }.map(&:key)
+      end
+
+      # Loads a bibtex file, given as parameter.
+      # Same functionality as initialize function, but allows to open
+      # multiple files sequentially.
+      def citations_load_bibtex_file(file)
+        app.config[:bibtex] = BibTeX.open(file)
+        app.config[:bibtex].replace
+        app.config[:bibtex].convert(:latex)
+      end
+
+      # Returns an array containing the years of the current
+      # bibtex file. e.g. useful to generate menu items.
+      def citations_list_years(search_key)
+        entries = config.bibtex.query(search_key)
+
+        years ||= []
+
+        entries.each do |entry|
+            next if entry.type == :string
+            years |= [entry.year.to_i]
+        end
+
+        years.sort { |x, y| x.to_i <=> y.to_i}.reverse
       end
 
       private
--- lib/middleman-citation/citations.rb
+++ lib/middleman-citation/citations.rb
@@ -10,24 +10,42 @@ module Citations
   class InvalidStyleError < StandardError
   end
 
-  def self.cite_full(key, bib, style, format='text')
+  # Returns a link to directly access scientific articles
+  # on the internet according to their digital object identifier (DOI).
+  def self.doi_link(doi)
+    "http://dx.doi.org/#{doi}"
+  end
+
+  def self.cite_full(key, bib, style, format='text', link=false)
     cp = CiteProc::Processor.new style: style, format: format
     cp.import bib.to_citeproc
+
+    citation = ""
+
     # need to convert latex special characters, like {\"o} to unicode
     #bib[key].convert_latex
     #puts bib[key]
     #CiteProc.process(bib[key].to_citeproc, :style => style)
     begin
-      cp.render(:bibliography, id: key).join("\n")
+        citation = cp.render(:bibliography, id: key).join("\n")
+
+        # Add optional link to reference.
+        if link == true
+            if !bib[key][:doi].nil? && !bib[key][:doi].empty?
+                citation << "&nbsp;<a href=\"#{doi_link(bib[key][:doi])}\">link</a>"
+            end
+        end
     rescue TypeError => e
       raise InvalidKeyError, "citation key '#{key}' not found"
     rescue CSL::ParseError => e
       raise InvalidStyleError, "citation style '#{style}' not found"
     end
+
+    return citation
   end
 
-  def self.cite_inline(key, bib, style)
-    "&nbsp;<span class='inline-citation'><span class='citation'>#{cite_full(key, bib, style,'html')}</span></span>"
+  def self.cite_inline(key, bib, style, link=false)
+    "&nbsp;<span class='inline-citation'><span class='citation'>#{cite_full(key, bib, style,'html',link)}</span></span>"
   end
 
 end
